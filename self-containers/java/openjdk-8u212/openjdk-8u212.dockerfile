d6619fbc59581bec4e9c462a0548cc49bd483b6a
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-08-12 20:22:29 -0300

    update: openjdk-8u212 is available

---
+++ self-containers/java/openjdk-8u212/openjdk-8u212.dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8u212
+
+WORKDIR /home/openjdk8u212
+
+RUN java -version
---
+++ self-containers/java/openjdk-8u212/simple-api-demo/src/main/java/com/huntercodexs/simpleapidemo/service/SimpleApiDemoService.java
@@ -0,0 +1,12 @@
+package com.huntercodexs.simpleapidemo.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleApiDemoService {
+
+    public ResponseEntity<?> service() {
+        return ResponseEntity.ok().body("OK, it is working fine !");
+    }
+}
